1feb9619c2a6ce0f535d8299a073a4e239e25276
Daniel <work.sotnik@gmail.com>
2020-02-29 15:14:58 +0300

    cubic ui upd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import "./styles.css";
 
+/* MAKE WRAPP */
+/* СОЗДАЕМ ОБЕРТКУ */
 let div = document.createElement('div');
-div.style.width = "300px";
-div.style.height = "300px";
+div.classList.add('cubic');
 document.body.prepend(div);
 
+/* ADD SVG */
+/* ПОДКЛЮЧАЕМ SVG */
 let svg = document.createElement('object');
 svg.setAttribute('id', 'bezier');
 svg.setAttribute('type', 'image/svg+xml');
 svg.setAttribute('data', 'src/line.svg');
 div.append(svg);
+
+let i = document.getElementById('bezier').contentDocument.body;
+console.log(i)
--- src/svg.js
+++ src/svg.js
@@ -9,10 +9,10 @@ let line = document.getElementById('line');
 
 /* INIT START PARAMS */
 /* ОБЪЯВЛЯЕМ НАЧАЛЬНОЕ ЗНАЧЕНИЕ */
-let x1 = 150;
-let y1 = 150;
-let x2 = 150;
-let y2 = 150;
+let x1 = 140;
+let y1 = 140;
+let x2 = 140;
+let y2 = 140;
 
 /* ITERATION FOR EVERY CIRCLE-TRIGGER */
 /* ЦИКЛ ПО КАЖДОМУ КРУГУ-ТРИГГЕРУ */
@@ -22,23 +22,43 @@ for (let circle of circles) {
             circle.setAttribute('cx', event.pageX);
             circle.setAttribute('cy', event.pageY);
 
-            if (event.target === circles[0]) { //if bttm trig, tho //если нижний триггер, то 
-                x1 = event.pageX;
-                y1 = event.pageY;
-            } else { //if not bttm tri, tho //если не ниж, то
-                x2 = event.pageX;
-                y2 = event.pageY;
+            if (event.pageX >= 300) { //overflow block //блок выхода за поля
+                circle.setAttribute('cx', "300");
+                if (event.target === circle[0]) x1 = 300;
+                if (event.target === circle[1]) x2 = 300;
+            } else if (event.pageX <= 0) {
+                circle.setAttribute('cx', "0");
+                if (event.target === circle[0]) x1 = 0;
+                if (event.target === circle[1]) x2 = 0;
+            } else if (event.pageY >= 300) {
+                circle.setAttribute('cy', "300");
+                if (event.target === circle[0]) y1 = 300;
+                if (event.target === circle[1]) y2 = 300;
+            } else if (event.pageY <= 0) {
+                circle.setAttribute('cy', "0");
+                if (event.target === circle[0]) y1 = 0;
+                if (event.target === circle[1]) y2 = 0;
+            } else {
+                if (event.target === circles[0]) { //if bttm trig, then //если нижний триггер, то 
+                    x1 = event.pageX;
+                    y1 = event.pageY;
+                } else if (event.target === circles[1]) { //if not bttm tri, then //если не ниж, то
+                    x2 = event.pageX;
+                    y2 = event.pageY;
+                }
             }
 
-            line.setAttribute("d", `M10 290 C ${x1} ${y1}, ${x2} ${y2}, 290 10`); //line coords //коорд линии
+            line.setAttribute("d", `M10 290 C ${x1} ${y1}, ${x2} ${y2}, 290 10`);
         };
 
         document.addEventListener('mousemove', onMouseMove); //concretize func //конкретизируем функ-ию
 
-        circle.onmouseup = () => {
-            document.removeEventListener('mousemove', onMouseMove); //remove conc func //убираем конк
-        };
+        circle.onmouseup = () => document.removeEventListener('mousemove', onMouseMove);
+        document.onmouseup = () => document.removeEventListener('mousemove', onMouseMove);
+        //remove conc func //убираем конк
     };
 
-    circle.ondragstart = () => false;
+    circle.ondragstart = () => false; //kill native event //убираем нативный ивент
 }
+
+console.log(document.parentNode);
